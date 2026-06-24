330bd1aa3f8a7ed4c23604e8a15a49aa1824b650
munazhairfan <munazhairfan@gmail.com>
2026-06-24 23:57:32 +0500

    feat: add auth, admin panel, checkout, and product features

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,12 +7,6 @@ declare global {
   } | undefined;
 }
 
-const MONGODB_URI = process.env.MONGO_URI;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGO_URI environment variable inside .env.local');
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -21,6 +15,12 @@ if (!MONGODB_URI) {
 const cached = global.mongoose || (global.mongoose = { conn: null, promise: null });
 
 async function dbConnect() {
+  const MONGODB_URI = process.env.MONGO_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGO_URI environment variable inside .env.local');
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
@@ -30,7 +30,7 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
